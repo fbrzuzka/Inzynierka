409723e9ea5e18549889d6beb764dc2f82ddd426
jknast <jknast@xdsnet.pl>
2024-07-23 23:57:47 +0200

    package as fat-jar to make possible run with java -jar

--- KuKuFi-autoPRK-Player/src/main/java/KuKuFi_Pi/pinout/PinoutServiceFactory.java
+++ KuKuFi-autoPRK-Player/src/main/java/KuKuFi_Pi/pinout/PinoutServiceFactory.java
@@ -1,14 +1,18 @@
 package KuKuFi_Pi.pinout;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.SystemUtils;
 
+@Slf4j
 public class PinoutServiceFactory {
 
     public PinoutService createService() {
         if (SystemUtils.IS_OS_LINUX
                 && !SystemUtils.OS_ARCH.equals("amd64")) { // raspberry
+            log.info("Create PinoutServicePi4J");
             return new PinoutServicePi4J();
         }
+        log.info("Create PinoutServiceDummy");
         return new PinoutServiceDummy();
     }
 }
